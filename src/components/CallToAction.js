41fecfa4b86a31fc489914a8aaa38341a62754e7
jake smith <Jacob_smith1223@me.com>
2022-07-13 21:29:24 -0600

    changed purple to blue

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -8,7 +8,7 @@ export function CallToAction() {
   return (
     <section
       id="get-started-today"
-      className="relative overflow-hidden bg-purple-600 py-32"
+      className="relative overflow-hidden bg-blue-600 py-32"
     >
       <Image
         className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
